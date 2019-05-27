f42bb2486ef55b91baa4f0c2b1b478dd49f83270
tuananhk13bk <tuananhk13bk@gmail.com>
2019-05-27 23:16:46 +0700

    Continue developing trips list page

--- client/src/actions/trip/tripActionTypes.ts
+++ client/src/actions/trip/tripActionTypes.ts
@@ -1,2 +1,5 @@
 export const SELECT_TRIP = "SELECT_TRIP";
 export const GET_ALL_TRIPS = "GET_ALL_TRIPS";
+
+export const GET_ALL_PROVINCES = "GET_ALL_PROVINCES";
+export const RECEIVE_TRIP_ERROR = "RECEIVE_TRIP_ERROR";
